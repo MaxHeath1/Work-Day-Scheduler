e42e835391b5ccdb5ca7ab7ab9cf8ea4bb565531
Maxwell Heath <maxheathbusiness@gmail.com>
2023-09-23 19:59:54 -0600

    Made the function to determine what the time of day is and to sort it based on the when it is scheduled using dayjs!

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-$(document).ready(function ()) {
+$(document).ready(function () {
     $("#currentDay").text(dayjs().format('MMMM D, YYYY'));
 
 function updateColors() {
     let currentHour = dayjs().hour();
+    $(".time-block").each(function () {
+        let blockHour = parseInt($(this).attr("id").split(".")[1]);
+        if (blockHour < currentHour) {
+            $(this).addClass ("past");
+        } else if (blockHour === currentHour) {
+            $(this).addClass ("present");
+        } else {
+            $(this).addClass ("future");
+        }
+    });
 }
 
+updateColors();
+
+
+$()
 
 
 
@@ -18,6 +32,4 @@ function updateColors() {
 
 
 
-
-
-}
+})
