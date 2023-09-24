c512f3e204985be81c6bd944c622b4ad1cdf47df
Maxwell Heath <maxheathbusiness@gmail.com>
2023-09-23 21:02:01 -0600

    Should be working but i wont know until tomorrow so i can see present and future

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
 $(document).ready(function () {
-    $("#currentDay").text(dayjs().format('MMMM D, YYYY'));
-    console.log
+    //Displays current time at the top of the page
+    $("#currentDay").text(dayjs().format('dddd, MMMM D HH:mm'));
 
-function updateColors() {
+    // Get the current hour
     let currentHour = dayjs().hour();
+
+    // Iterate over each time-block
     $(".time-block").each(function () {
-        let blockHour = parseInt($(this).attr("id").split(".")[1]);
+        // Extract the hour from the id of the block
+        let blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+        // Compare the block hour with the current hour and add appropriate class
         if (blockHour < currentHour) {
-            $(this).addClass ("past");
+            $(this).addClass("past");
         } else if (blockHour === currentHour) {
-            $(this).addClass ("present");
+            $(this).addClass("present");
         } else {
-            $(this).addClass ("future");
+            $(this).addClass("future");
         }
-        console.log
     });
-}
 
-updateColors();
-
-
-$(".time-block").each(function () {
-    let id = $(this).attr("id");
-    let savedText = localStorage.getItem(id);
-    if (savedText) {
-        $(this).find(".description").val(savedText);
-    }
-    console.log
-});
-
-$(".saveBtn").on("click", function() {
-let blockId = $(this).parent().attr("id");
-let textAreaValue = $(this).siblings(".description").val();
-localStorage.setItem(blockId, textAreaValue)
-console.log
-});
+$(".saveBtn").on("click", function () {
+    let blockId = $(this).parent().attr("id");
+    let textAreaValue = $(this).siblings(".description").val();
+    localStorage.setItem(blockId, textAreaValue);
+  });
 });
