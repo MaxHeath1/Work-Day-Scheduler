39541599472f76a9ebc25141b1be5870175b8281
Maxwell Heath <maxheathbusiness@gmail.com>
2023-09-27 18:35:19 -0600

    Added more to readme

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 $(document).ready(function () {
-    //Displays current time at the top of the page
     $("#currentDay").text(dayjs().format('dddd, MMMM D HH:mm'));
 
-    // Get the current hour
     let currentHour = dayjs().hour();
 
-    // Iterate over each time-block
     $(".time-block").each(function () {
-        // Extract the hour from the id of the block
         let blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-        // Compare the block hour with the current hour and add appropriate class
         if (blockHour < currentHour) {
             $(this).addClass("past");
         } else if (blockHour === currentHour) {
